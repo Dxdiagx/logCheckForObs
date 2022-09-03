a2ab21e814f8a8c88226edae749fdcce5d5de980
dxdiagx <acarsamet1803@gmail.com>
2022-09-03 18:23:36 +0300

    Samet Acar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,20 +8,20 @@ public class Main {
        Students students1=new Students();
        students1.schoolNo="123456789";
        students1.pass="123456789";
-       students1.firstName="Mehmet";
+       students1.firstName="Samet";
 
         Students students2= new Students();
         students2.schoolNo="123456789";
-        students2.pass="123456789";
-        students2.firstName="Ali";
+        students2.pass="1234567890";
+        students2.firstName="Mehmet";
         Teachers teachers1=new Teachers();
         teachers1.tC="12345678901";
         teachers1.pass="123456";
-        teachers1.firstName="Samet";
+        teachers1.firstName="Ahmet";
         Teachers teachers2=new Teachers();
         teachers2.tC="12345678901";
-        teachers2.pass="123456";
-        teachers2.firstName="Veli";
+        teachers2.pass="1234567";
+        teachers2.firstName="Taner";
 
         LogCheckManager logCheckManager=new LogCheckManager(students1);
         logCheckManager.loginCheck(students1);
